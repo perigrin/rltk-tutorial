2639c163d24977a3c8a1a3d0a339d07775b5bbf5
Chris Prather <chris@prather.org>
2021-01-18 23:54:28 -0500

    chapter 42

--- src/rex_assets.rs
+++ src/rex_assets.rs
@@ -1,22 +1,22 @@
-use rltk::{rex::XpFile};
+use rltk::rex::XpFile;
 
-rltk::embedded_resource!(SMALL_DUNGEON, "../../resources/SmallDungeon_80x50.xp");
-rltk::embedded_resource!(WFC_DEMO_IMAGE1, "../../resources/wfc-demo1.xp");
-rltk::embedded_resource!(WFC_POPULATED, "../../resources/wfc-populated.xp");
+rltk::embedded_resource!(SMALL_DUNGEON, "../resources/SmallDungeon_80x50.xp");
+rltk::embedded_resource!(WFC_DEMO_IMAGE1, "../resources/wfc-demo1.xp");
+rltk::embedded_resource!(WFC_POPULATED, "../resources/wfc-populated.xp");
 
 pub struct RexAssets {
-    pub menu : XpFile
+    pub menu: XpFile,
 }
 
 impl RexAssets {
     #[allow(clippy::new_without_default)]
     pub fn new() -> RexAssets {
-        rltk::link_resource!(SMALL_DUNGEON, "../../resources/SmallDungeon_80x50.xp");
-        rltk::link_resource!(WFC_DEMO_IMAGE1, "../../resources/wfc-demo1.xp");
-        rltk::link_resource!(WFC_POPULATED, "../../resources/wfc-populated.xp");
+        rltk::link_resource!(SMALL_DUNGEON, "../resources/SmallDungeon_80x50.xp");
+        rltk::link_resource!(WFC_DEMO_IMAGE1, "../resources/wfc-demo1.xp");
+        rltk::link_resource!(WFC_POPULATED, "../resources/wfc-populated.xp");
 
-        RexAssets{
-            menu : XpFile::from_resource("../../resources/SmallDungeon_80x50.xp").unwrap()
+        RexAssets {
+            menu: XpFile::from_resource("../resources/SmallDungeon_80x50.xp").unwrap(),
         }
     }
 }
